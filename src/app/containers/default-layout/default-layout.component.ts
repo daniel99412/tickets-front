42914543d4d0b4f4198eb242a92e2142aaf18427
daniel99412 <daniel.pardo194@gmail.com>
2020-11-12 10:23:01 -0600

    modifiedDefaultLayoutAddedProfileAndChangePassword

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 import { navItems } from '../../_nav';
 
 @Component({
@@ -8,9 +9,12 @@ import { navItems } from '../../_nav';
   styleUrls: ['./default-layout.component.scss']
 })
 export class DefaultLayoutComponent implements OnInit {
+  @ViewChild('asideMenuTabs') asideTabs: TabsetComponent;
+
   public sidebarMinimized = false;
   public navItems = navItems;
   public user;
+  public isShowing = false;
 
   constructor(private router: Router) {}
 
@@ -27,5 +31,16 @@ export class DefaultLayoutComponent implements OnInit {
   logout() {
     sessionStorage.clear();
     this.router.navigate(['login']);
+  }
+
+  showAside() {
+    if (!document.getElementsByClassName('app')[0].classList.value.includes('aside-menu-lg-show')) {
+      this.isShowing = true;
+      this.asideTabs.tabs[0].active = true;
+      document.getElementsByClassName('app')[0].classList.add('aside-menu-lg-show');
+    } else {
+      this.isShowing = false;
+      document.getElementsByClassName('app')[0].classList.remove('aside-menu-lg-show');
+    }
   }
 }
