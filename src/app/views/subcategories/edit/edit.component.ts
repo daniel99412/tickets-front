50dc0da6b7e654c8951a2d9e23e3ecedd63b9f75
JosueMarquez <JosueMarquez@bitbucket.org>
2020-11-19 16:22:35 -0600

    Finish edit general

--- src/app/views/subcategories/edit/edit.component.ts
+++ src/app/views/subcategories/edit/edit.component.ts
@@ -1,5 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap/modal';
+import { Subcategory } from '../../../models/subcategory';
+import { SubcategoryService } from '../../../services/subcategory.service';
+import { ToastrService } from 'ngx-toastr';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-subcategory',
@@ -13,20 +17,51 @@ export class EditComponent implements OnInit {
   @Output() subcategoryEdited = new EventEmitter<any>();
 
   subcategory;
+  category;
+  categorySelected = null;
+  subcategoryName = null;
 
-  constructor() { }
+  constructor(
+    private subcategoryService: SubcategoryService,
+    private toastrService: ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
 
   show(subcategory) {
     this.subcategory = subcategory;
+    this.categorySelected = subcategory.category;
+    console.log(this.categorySelected, "Completo");
     this.edit.show();
+  }
+
+  update() {
+    this.categorySelected;
+    const subcategory = new Subcategory(this.subcategory._id, this.subcategory.name, this.categorySelected);
+    this.subcategoryService.update(this.subcategory._id, subcategory)
+      .subscribe(subcategorySaved => {
+        this.toastrService.success('Subcategoria creada', '¡Éxito!');
+        this.cancel();
+        this.subcategoryEdited.emit(subcategorySaved);
+      },
+        err => {
+          this.cancel();
+          this.toastrService.error(err.error.message, '¡Error!');
+        });
+
+    this.edit.hide();
   }
 
   compareFn(obj1, obj2): boolean {
     console.log(obj1);
     console.log(obj2);
     return obj1 === obj2;
+  }
+
+  cancel() {
+    this.subcategoryName = null;
+    this.categorySelected = null;
+    this.edit.hide();
   }
 }
