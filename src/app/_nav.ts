975db4f5f67cef74b811eee3d96bbcd10c909db0
Daniel Pardo <daniel.pardo194@gmail.com>
2021-06-25 14:29:56 -0500

    fix

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -4,37 +4,58 @@ export const navItems: INavData[] = [
   {
     name: 'Dashboard',
     url: '/dashboard',
-    icon: 'icon-speedometer'
+    icon: 'icon-speedometer',
+    attributes: {
+      'minAccess': 'usuario'
+    }
   },
   {
     name: 'Tickets',
     url: '/tickets/list',
-    icon: 'icon-note'
+    icon: 'icon-note',
+    attributes: {
+      'minAccess': 'usuario'
+    }
   },
   {
     name: 'Usuarios',
     url: '/users',
-    icon: 'icon-user'
+    icon: 'icon-user',
+    attributes: {
+      'minAccess': 'jefe'
+    }
   },
   {
     name: 'Roles',
     url: '/roles',
-    icon: 'icon-book-open'
+    icon: 'icon-book-open',
+    attributes: {
+      'minAccess': 'jefe'
+    }
   },
   {
     name: 'Sucursales',
     url: '/branchOffices',
-    icon: 'icon-home'
+    icon: 'icon-home',
+    attributes: {
+      'minAccess': 'gerente'
+    }
   },
   {
     name: 'Categorias',
     url: '/categories',
-    icon: 'icon-list'
+    icon: 'icon-list',
+    attributes: {
+      'minAccess': 'gerente'
+    }
   },
   {
     name: 'Subcategorias',
     url: '/subcategories',
-    icon: 'icon-organization'
+    icon: 'icon-organization',
+    attributes: {
+      'minAccess': 'gerente'
+    }
   },
   // {
   //   name: 'Asignacion de Rol',
