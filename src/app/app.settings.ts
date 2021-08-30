ae7b80cfe7cd64328588bccb0888e26ddaf40cf2
Daniel Pardo <daniel.pardo194@gmail.com>
2021-08-30 18:27:54 -0500

    fix

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static readonly API = 'http://52.14.132.150:3800/api/v1';
-    public static readonly SOCKETURL = "http://52.14.132.150:3800";
+    public static readonly API = 'http://54.91.130.246:3800/api/v1';
+    public static readonly SOCKETURL = "http://54.91.130.246:3800";
    
 }
